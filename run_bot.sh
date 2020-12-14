17f566489a645599846a011037a3ec2774e36a25
Vladimir Severov <v.severov@corp.mail.ru>
2020-12-14 11:43:43 +0300

    Make default value for log files in shell scripts

--- run_bot.sh
+++ run_bot.sh
@@ -2,9 +2,12 @@
 
 set -e
 
-export LOG_FILE=""
 while [[ $# -gt 0 ]]; do
     case "$1" in
+    -f)
+        FILE_MODE=true
+        shift
+        ;;
     -l | --log_file)
         LOG_FILE="$2"
         shift
@@ -17,10 +20,15 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
+if [ -n "${FILE_MODE}" ] && [ -z "${LOG_FILE}" ]; then
+    LOG_FILE="archive/bot.log"
+fi
+
+cd "$(dirname "$0")"
 source venv/bin/activate
 if [ -z "${LOG_FILE}" ]; then
     python3 bot.py
 else
     mkdir -p "$(dirname "${LOG_FILE}")"
-    python3 bot.py >"${LOG_FILE}"
+    python3 bot.py &>"${LOG_FILE}"
 fi
--- run_check.sh
+++ run_check.sh
@@ -2,9 +2,12 @@
 
 set -e
 
-export LOG_FILE=""
 while [[ $# -gt 0 ]]; do
     case "$1" in
+    -f)
+        FILE_MODE=true
+        shift
+        ;;
     -l | --log_file)
         LOG_FILE="$2"
         shift
@@ -17,10 +20,15 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
+if [ -n "${FILE_MODE}" ] && [ -z "${LOG_FILE}" ]; then
+    LOG_FILE="archive/$(date +"%Y%m%d_%H%M%S").log"
+fi
+
+cd "$(dirname "$0")"
 source venv/bin/activate
 if [ -z "${LOG_FILE}" ]; then
     python3 check.py -v
 else
     mkdir -p "$(dirname "${LOG_FILE}")"
-    python3 check.py >"${LOG_FILE}"
+    python3 check.py &>"${LOG_FILE}"
 fi
