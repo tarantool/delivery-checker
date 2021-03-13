e90efd4fc70edfda5ee3ffbf2bc08327d8dba9ef
Vladimir Severov <v.severov@corp.mail.ru>
2021-03-13 04:48:52 +0300

    Fix .gitignore and remove redundant scripts

--- scripts/install/amazon-linux_script_1.10.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Exit when any command in script file fails
-set -e
-
-# Installation commands
-
-curl -L https://tarantool.io/release/1.10/installer.sh | bash
-sudo yum install tarantool
