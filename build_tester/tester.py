5cd047048bfee1f9d48bc33039e38f7a3b05bef1
Nick Volynkin <nick.volynkin@gmail.com>
2022-06-20 11:10:12 +0700

    Introduce a CheckerConfig class
    Store all configuration values in the CheckerConfig.
    Set configuration values in the following order:
    
    1. CLI arguments.
    2. Configuration values in a .json configuration file.
    3. Default values.
    
    Add the following CLI arguments, matching the already existing
    values from config.json:
    
    --commands-url sets URL for fetching Tarantool build instructions
    --commands-url-user sets the user for auth on the above URL
    --commands-url-pass sets the password
    
    Part of #45

--- build_tester/tester.py
+++ build_tester/tester.py
@@ -8,70 +8,26 @@ import requests
 from build_tester.builders.docker_builder import DockerBuilder, DockerInfo
 from build_tester.builders.virtual_box import VirtualBoxBuilder, VirtualBoxInfo
 from build_tester.results_sync import ResultsManager, Result
+from config.config import CheckerConfig
 
 
 class Tester:
-    def __init__(self, config_path='./config.json', console_mode=False, debug_mode=False):
-        self.__console_mode = console_mode
-        self.__debug_mode = debug_mode
-        with open(config_path, 'r') as fs:
-            config = json.load(fs)
-            self.__parse_config(config)
+
+    def __init__(self, config: CheckerConfig):
+        self.config: CheckerConfig = config
 
         self.__logs = []
         self.__builds = None
         self.__all_builds = []
 
-        self.__results_manager = ResultsManager(config)
-
-    def __parse_config(self, config):
-        self.commands_url = config.get('commands_url', 'https://www.tarantool.io/api/tarantool/info/versions/')
-        self.commands_url_user = config.get('commands_url_user')
-        self.commands_url_pass = config.get('commands_url_pass')
-
-        self.__scripts_dir_path = config.get('scripts_dir_path', './scripts')
-
-        self.__prepare_dir_name = config.get('prepare_dir_name', 'prepare')
-        self.__prepare_dir_path = os.path.join(self.__scripts_dir_path, self.__prepare_dir_name)
-
-        self.__install_dir_name = config.get('install_dir_name', 'install')
-        self.__install_dir_path = os.path.join(self.__scripts_dir_path, self.__install_dir_name)
-
-        self.__local_dir_path = config.get('local_dir_path', './local')
-        self.__remote_dir_path = config.get('remote_dir_path', './remote')
-        self.__archive_dir_path = config.get('archive_dir_path', './archive')
-
-        self.__logs_dir_name = config.get('logs_dir_name', 'logs')
-        self.__logs_dir_path = os.path.join(self.__local_dir_path, self.__logs_dir_name)
-
-        self.__tests_dir_name = config.get('tests_dir_name', 'tests')
-        self.__tests_dir_path = os.path.join(self.__local_dir_path, self.__tests_dir_name)
-
-        self.__results_file_name = config.get('results_file_name', 'results.json')
-        self.__results_file_path = os.path.join(self.__local_dir_path, self.__results_file_name)
-
-        self.__default_use_cache = config.get('default_use_cache', False)
-
-        os_params = config.get('os_params')
-        assert config.get('os_params') is not None, 'No OS params in config!'
-
-        self.__docker_params = {
-            k: v['docker']
-            for k, v in os_params.items()
-            if v.get('docker') is not None
-        }
-        self.__virtual_box_params = {
-            k: v['virtual_box']
-            for k, v in os_params.items()
-            if v.get('virtual_box') is not None
-        }
+        self.__results_manager = ResultsManager(config=config)
 
     def __log(self, msg):
         self.__logs.append(msg)
 
     def __save_logs(self, path=None):
         if path is None:
-            path = os.path.join(self.__logs_dir_path, f'last.log')
+            path = os.path.join(self.config.logs_dir_path, f'last.log')
 
         with open(path, mode='w') as fs:
             logs = '\n'.join(map(lambda x: str(x), self.__logs))
@@ -93,9 +49,9 @@ class Tester:
     def __download_scripts(self):
         commands_response: requests.Response
 
-        auth_user = self.commands_url_user
-        auth_pass = self.commands_url_pass
-        url = self.commands_url
+        auth_user = self.config.commands_url_user
+        auth_pass = self.config.commands_url_pass
+        url = self.config.commands_url
 
         if auth_user and auth_pass:
             commands_response = requests.get(url, auth=(auth_user, auth_pass))
@@ -119,12 +75,12 @@ class Tester:
             print(f'unknown error when parsing site response: {err}')
 
         # Remove old scripts
-        for file in os.listdir(self.__install_dir_path):
+        for file in os.listdir(self.config.install_dir_path):
             if file != 'default.sh':
-                os.remove(os.path.join(self.__install_dir_path, file))
+                os.remove(os.path.join(self.config.install_dir_path, file))
 
         # Get base of install scripts
-        with open(os.path.join(self.__install_dir_path, 'default.sh'), mode='r') as fs:
+        with open(os.path.join(self.config.install_dir_path, 'default.sh'), mode='r') as fs:
             default_script = fs.read()
 
         builds = []
@@ -141,28 +97,28 @@ class Tester:
                 # This is to avoid running Docker in Docker or Docker in VirtualBox
                 if 'docker' not in os_name:
                     builds += DockerBuilder.get_builds(
-                        self.__docker_params,
+                        self.config.docker_params,
                         os_name, build_name,
-                        self.__default_use_cache,
+                        self.config.default_use_cache,
                     )
                     builds += VirtualBoxBuilder.get_builds(
-                        self.__virtual_box_params,
+                        self.config.virtual_box_params,
                         os_name, build_name,
                     )
 
                 # Find name of image in commands and use it
                 else:
                     builds += DockerBuilder.get_docker_builds(
-                        self.__docker_params,
+                        self.config.docker_params,
                         os_name, build_name,
-                        commands, self.__default_use_cache,
+                        commands, self.config.default_use_cache,
                     )
                     commands = []
 
-                if len(builds) == builds_count and self.__debug_mode:
+                if len(builds) == builds_count and self.config.debug_mode:
                     print(f'OS: {os_name}. Build: {build_name}. {Result.NO_TEST.value}')
 
-                path = os.path.join(self.__install_dir_path, f'{os_name}_{build_name}.sh')
+                path = os.path.join(self.config.install_dir_path, f'{os_name}_{build_name}.sh')
                 with open(path, mode='w') as fs:
                     fs.write(default_script)
                     fs.write('\n'.join(commands))
@@ -172,10 +128,10 @@ class Tester:
         return builds
 
     def test_builds(self):
-        shutil.rmtree(self.__local_dir_path, ignore_errors=True)
+        shutil.rmtree(self.config.local_dir_path, ignore_errors=True)
 
-        os.makedirs(self.__tests_dir_path)
-        os.makedirs(self.__logs_dir_path)
+        os.makedirs(self.config.tests_dir_path)
+        os.makedirs(self.config.logs_dir_path)
 
         canceled = False
         self.__results = {}
@@ -187,13 +143,13 @@ class Tester:
             os_name = self.__get_build_os_name(build)
 
             log_prefix = f'OS: {os_name}. Build: {build.build_name}.'
-            if self.__console_mode:
+            if self.config.console_mode:
                 print(f'\r{log_prefix} Running...', end='')
             else:
                 print(f'{log_prefix} ', end='')
 
             self.__results[os_name] = self.__results.get(os_name, {})
-            install_logs_path = os.path.join(self.__logs_dir_path, f'{os_name}_{build.build_name}.log')
+            install_logs_path = os.path.join(self.config.logs_dir_path, f'{os_name}_{build.build_name}.log')
             start = time.time()
 
             try:
@@ -206,19 +162,19 @@ class Tester:
                     if isinstance(build, DockerInfo):
                         docker_builder = DockerBuilder(
                             build_info=build,
-                            scripts_dir_path=self.__scripts_dir_path,
-                            prepare_dir_path=self.__prepare_dir_path,
-                            tests_dir_path=self.__tests_dir_path,
+                            scripts_dir_path=self.config.scripts_dir_path,
+                            prepare_dir_path=self.config.prepare_dir_path,
+                            tests_dir_path=self.config.tests_dir_path,
                             log_func=self.__log,
                         )
                         deploy_result = docker_builder.deploy()
                     elif isinstance(build, VirtualBoxInfo):
                         virtual_box_builder = VirtualBoxBuilder(
                             build_info=build,
-                            scripts_dir_path=self.__scripts_dir_path,
-                            prepare_dir_path=self.__prepare_dir_path,
-                            install_dir_path=self.__install_dir_path,
-                            tests_dir_path=self.__tests_dir_path,
+                            scripts_dir_path=self.config.scripts_dir_path,
+                            prepare_dir_path=self.config.prepare_dir_path,
+                            install_dir_path=self.config.install_dir_path,
+                            tests_dir_path=self.config.tests_dir_path,
                             log_func=self.__log,
                         )
                         deploy_result = virtual_box_builder.deploy()
@@ -238,7 +194,7 @@ class Tester:
                 if result == Result.OK:
                     is_results_ok = False
 
-                    path = os.path.join(self.__tests_dir_path, f'{os_name}_{build.build_name}.json')
+                    path = os.path.join(self.config.tests_dir_path, f'{os_name}_{build.build_name}.json')
                     if os.path.exists(path):
                         with open(path) as fs:
                             try:
@@ -262,13 +218,13 @@ class Tester:
                 result = Result.ERROR
                 self.__save_logs(install_logs_path)
 
-            if self.__console_mode:
+            if self.config.console_mode:
                 print(f'\r{log_prefix} ', end='')
             print(f'Elapsed time: {time.time() - start:.2f}. {result.value}')
 
             self.__results[os_name][build.build_name] = result
 
-        with open(self.__results_file_path, mode='w') as fs:
+        with open(self.config.results_file_path, mode='w') as fs:
             fs.write(json.dumps(self.__results))
 
     def find_lost_results(self):
